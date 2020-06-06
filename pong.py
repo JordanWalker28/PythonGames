95259919cbb3267307e446cfeef11b53d8131b43
Jordan Walker <jordanswalker28@gmail.com>
2020-06-06 17:26:45 +0100

    Added mechanics to allow for scoring

--- pong.py
+++ pong.py
@@ -1,5 +1,6 @@
 import pygame
 from paddle import Paddle
+from ball import Ball
 
 pygame.init()
 
@@ -19,14 +20,22 @@ paddleB = Paddle(GREEN, 10, 100)
 paddleB.rect.x = 670
 paddleB.rect.y = 200
 
+ball = Ball(WHITE,10,10)
+ball.rect.x = 345
+ball.rect.y = 195
+
 all_sprites_list = pygame.sprite.Group()
 
 all_sprites_list.add(paddleA)
 all_sprites_list.add(paddleB)
+all_sprites_list.add(ball)
 
 GameActive = True
  
 clock = pygame.time.Clock()
+
+scoreA = 0
+scoreB = 0
 
 while GameActive:
 
@@ -45,13 +54,33 @@ while GameActive:
         paddleB.moveDown(5)
         
     all_sprites_list.update()
+    
+    if ball.rect.x>=690:
+        scoreA+=1
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.x<=0:
+        scoreB+=1
+        ball.velocity[0] = -ball.velocity[0]
+    if ball.rect.y>490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y<0:
+        ball.velocity[1] = -ball.velocity[1]
 
+    if pygame.sprite.collide_mask(ball, paddleA) or pygame.sprite.collide_mask(ball, paddleB):
+      ball.bounce()
+      
     screen.fill(BLACK)
 
     pygame.draw.line(screen, GREEN, [349, 0], [349, 500], 5)
     pygame.draw.line(screen, WHITE, [0, 1], [700, 1], 5)
 
     all_sprites_list.draw(screen)
+    
+    font = pygame.font.Font(None, 74)
+    text = font.render(str(scoreA), 1, WHITE)
+    screen.blit(text, (250,10))
+    text = font.render(str(scoreB), 1, WHITE)
+    screen.blit(text, (420,10))
     
     pygame.display.flip()
      
