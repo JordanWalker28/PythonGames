49def87e7cf1282f3d980809d4314d0fb891f31e
Jordan Walker <jordanswalker28@gmail.com>
2020-06-23 00:09:39 +0100

    Made some alterations

--- dinoClone.py
+++ dinoClone.py
@@ -1,68 +1,209 @@
 import pygame
-from dinosaur import Dinosaur
-from obstacle import Obstacle
+ 
+# Global constants
+ 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ 
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ 
+ 
+class Player(pygame.sprite.Sprite):
 
-pygame.init()
+    def __init__(self):
 
-size = width, height = 800, 600
-gameDisplay = pygame.display.set_mode(size)
+        super().__init__()
+ 
+        width = 40
+        height = 60
+        self.image = pygame.Surface([width, height])
+        self.image.fill(RED)
 
-xPos = 0
-yPos = 0
-black = 0,0,0
-Ground_Height = height - 100
+        self.rect = self.image.get_rect()
 
-dinsaur = Dinosaur(Ground_Height)
+        self.change_x = 0
+        self.change_y = 0
+ 
+        self.level = None
+ 
+    def update(self):
 
-lastFrame = pygame.time.get_ticks()
+        self.calc_grav()
 
-import random
-MINGAP = 200
-VELOCITY = 300
-MAXGAP = 600
+        self.rect.x += self.change_x
 
-obstacles = []
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
 
-num_of_obstacles = 4
-lastObstacle = width
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                self.rect.left = block.rect.right
 
-obstaclesize = 20
+        self.rect.y += self.change_y
+ 
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
 
-for i in  range(4):
-    lastObstacle += MINGAP + (MAXGAP - MINGAP) * random.random()
-    obstacles.append(Obstacle(lastObstacle,size,Ground_Height))
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
+ 
+            self.change_y = 0
+ 
+    def calc_grav(self):
+        if self.change_y == 0:
+            self.change_y = 1
+        else:
+            self.change_y += .35
 
-white = 255,255,255
+        if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
+            self.change_y = 0
+            self.rect.y = SCREEN_HEIGHT - self.rect.height
+ 
+    def jump(self):
 
-while True:
-    t = pygame.time.get_ticks()
-    deltaTime = (t-lastFrame)/1000.0
-    lastFrame = t
+        self.rect.y += 2
+        platform_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        self.rect.y -= 2
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                dinsaur.jump()
+        if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
+            self.change_y = -10
+ 
+    def go_left(self):
+        self.change_x = -6
+ 
+    def go_right(self):
+        self.change_x = 6
+ 
+    def stop(self):
+        self.change_x = 0
+ 
+ 
+class Platform(pygame.sprite.Sprite):
+ 
+    def __init__(self, width, height):
+        super().__init__()
+ 
+        self.image = pygame.Surface([width, height])
+        self.image.fill(GREEN)
+ 
+        self.rect = self.image.get_rect()
+ 
+ 
+class Level(object):
 
-    gameDisplay.fill(black)
+    def __init__(self, player):
 
-    dinsaur.update(deltaTime)
-    dinsaur.draw(gameDisplay)
+        self.platform_list = pygame.sprite.Group()
+        self.enemy_list = pygame.sprite.Group()
+        self.player = player
+  
+        self.background = None
+ 
+    def update(self):
+        self.platform_list.update()
+        self.enemy_list.update()
+ 
+    def draw(self, screen):
+  
+        screen.fill(BLUE)
 
-    for obs in obstacles:
-        obs.update(deltaTime, VELOCITY)
-        obs.draw(gameDisplay)
-        if (obs.checkOver()):
-            lastObstacle += MINGAP + (MAXGAP - MINGAP) * random.random()
-            obs.x = lastObstacle
+        self.platform_list.draw(screen)
+        self.enemy_list.draw(screen)
+ 
+ 
 
-    lastObstacle -= VELOCITY * deltaTime
+class Level_01(Level):
 
-    pygame.draw.rect(gameDisplay, white, [0,Ground_Height , width, height - Ground_Height])
-    pygame.draw.rect(gameDisplay, white, [xPos, yPos, 40, 50])
-    xPos += 1
-    yPos += 1
-    pygame.display.update()  # updates the screen 
+    def __init__(self, player):
+
+        Level.__init__(self, player)
+
+        level = [[210, 70, 500, 500],
+                 [210, 70, 200, 400],
+                 [210, 70, 600, 300],
+                 ]
+                 
+        for platform in level:
+            block = Platform(platform[0], platform[1])
+            block.rect.x = platform[2]
+            block.rect.y = platform[3]
+            block.player = self.player
+            self.platform_list.add(block)
+ 
+ 
+def main():
+    pygame.init()
+
+    size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+    screen = pygame.display.set_mode(size)
+ 
+    pygame.display.set_caption("Platformer Jumper")
+
+    player = Player()
+ 
+    level_list = []
+    level_list.append( Level_01(player) )
+
+    current_level_no = 0
+    current_level = level_list[current_level_no]
+ 
+    active_sprite_list = pygame.sprite.Group()
+    player.level = current_level
+ 
+    player.rect.x = 340
+    player.rect.y = SCREEN_HEIGHT - player.rect.height
+    active_sprite_list.add(player)
+ 
+    done = False
+ 
+    clock = pygame.time.Clock()
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+ 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.go_left()
+                if event.key == pygame.K_RIGHT:
+                    player.go_right()
+                if event.key == pygame.K_UP:
+                    player.jump()
+ 
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT and player.change_x < 0:
+                    player.stop()
+                if event.key == pygame.K_RIGHT and player.change_x > 0:
+                    player.stop()
+ 
+        active_sprite_list.update()
+ 
+        current_level.update()
+ 
+        if player.rect.right > SCREEN_WIDTH:
+            player.rect.right = SCREEN_WIDTH
+ 
+        if player.rect.left < 0:
+            player.rect.left = 0
+ 
+        current_level.draw(screen)
+        active_sprite_list.draw(screen)
+ 
+        clock.tick(60)
+ 
+        pygame.display.flip()
+ 
+    pygame.quit()
+ 
+if __name__ == "__main__":
+    main()
